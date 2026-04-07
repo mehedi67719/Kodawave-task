d4161f6bbd286553f1b87587998b510c02e9e941
Mehedi Hassan <meh67719@gamil.com>
2026-04-07 10:46:30 +0600

    install react router and tailwind css

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className='min-h-screen '>
+           <h1 className='text-2xl font-bold'> this is home page</h1>
+        </div>
+    );
+};
+
+export default Home;
